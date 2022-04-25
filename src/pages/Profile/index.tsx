636c4e939647fddc84bbf7e36c1c3129f280cc8b
Eduardo <eduardobrumassio@hotmail.com>
2022-04-25 16:33:30 -0300

    Create ProfileData

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Container, Main, LeftSide, RightSide } from './styles';
 
-// import { Container } from './styles';
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
     return (
         <Container>
             <Main>
-                <LeftSide></LeftSide>
+                <LeftSide>
+                    <ProfileData
+                        username={'eduardobrumassio'}
+                        name={'Eduardo Brumassio'}
+                        avatarUrl={'https://media-exp1.licdn.com/dms/image/C4D03AQEetF4xXW18zg/profile-displayphoto-shrink_100_100/0/1600270160169?e=1656547200&v=beta&t=rDz_tsKYiVpWGHzE4TeUTrijyzlgNYIZvdXX_mJdPuM'}
+                        followers={900}
+                        following={7}
+                        company={'Aspect'}
+                        location={'Paraná, Brazil'}
+                        email={'eduardobrumassio@hotmail.com'}
+                        blog=""
+                    />
+                </LeftSide>
                 <RightSide></RightSide>
             </Main>
         </Container>
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,11 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    --horizontal-padding: 16px;
+    --vertical-padding: 24px;
+
+    padding: var(--vertical-padding) var(--horizontal-padding);
 `;
 
-export const Main = styled.div``;
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
 
-export const LeftSide = styled.div``;
+    margin: 0 auto;
+    max-width: 1280px;
 
-export const RightSide = styled.div``;
+    @media (min-width: 768) {
+        flex-direction: row;
+    }
+`;
+
+export const LeftSide = styled.div`
+    @media (min-width: 768){
+        width: 25%;
+    }
+`;
+
+export const RightSide = styled.div`
+    @media (min-width: 768){
+        width: 75%;
+    }
+`;
